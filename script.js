1e0d8cec37c81269b8c2ecce3874e062154941b7
TripleKdev <80637250+kangkengkhadev@users.noreply.github.com>
2025-08-14 21:06:45 +0700

    fix script

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ let c = 5 ;
 console.log(c);
 console.log("Hello World");
 console.log("This is a test script.");
-console.log("End of script.");              
+console.log("End of script.");        
+console.log("This is a new line added to the script."); 
+console.log("Another line for testing purposes.");  
+console.log("Final line to conclude the script.");      
